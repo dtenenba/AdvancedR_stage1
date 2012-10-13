49ec97e98d2ab8df05b9c4ec3d85f9cc9428c35c
dtenenba <dtenenba@fhcrc.org>
2012-10-13 15:32:02 -0700

    first commit

--- README.md
+++
@@ -1,4 +0,0 @@
-AdvancedR_stage1
-================
-
-Demo script-to-package for Advanced R Course, Seattle, October 2012, stage 1
---
+++ yeastCorrelatedExpression.R
@@ -0,0 +1,29 @@
+correlationFinder <- function  () {
+  dataFile="sub_combined_complete_dataset_526G_198E.txt"
+  cor.threshold <- 0.85
+
+  tbl <- read.table(dataFile, sep='\t', header=T, quote='',
+    comment.char='', fill=T, stringsAsFactors=FALSE)
+  rownames(tbl) <- tbl$X
+  exclude.these.columns <-  which(sapply(1:ncol(tbl),
+                                    function(col) class(tbl [,col])) != 'numeric')
+  if(length(exclude.these.columns) > 0)
+    tbl <- tbl [, -exclude.these.columns]
+  mtx.cor <- cor(t(as.matrix(tbl)), use='pairwise.complete.obs')
+  mtx.cor <- upper.tri(mtx.cor) * mtx.cor
+  max = nrow(mtx.cor)
+
+  correlated.genes <- list()
+  ret <- list()
+  for(r in 1:max) {
+    zz = as.integer(which(mtx.cor [r,] > cor.threshold))
+    if(length(zz) > 0) {
+      gene.a = rownames(mtx.cor) [r]
+      genes.b = rownames(mtx.cor) [zz]
+      correlated.genes [[gene.a]] <- genes.b
+      ret[[ rownames(mtx.cor)[r] ]] <-
+        rownames(mtx.cor)[zz]
+      } # if length
+     } # for r
+  ret
+}
